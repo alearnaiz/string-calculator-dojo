cdb36727010b6a02a43ca98595f4f3b7ec34b3ec
Alejandro Arnáiz <alejandro.arnaiz.gonzalez@gmail.com>
2017-01-19 13:09:43 +0100

    Use \\ instead of \

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -10,7 +10,7 @@ public class StringCalculator {
 
     private static final String START_CUSTOM_DELIMITERS = "//";
 
-    private static final String END_CUSTOM_DELIMITERS = "\n";
+    private static final String END_CUSTOM_DELIMITERS = "\\n";
 
     private static final String START_CUSTOM_DELIMITER = "[";
 
@@ -24,7 +24,7 @@ public class StringCalculator {
 
         // Default values
         delimiters.add(",");
-        delimiters.add("\n");
+        delimiters.add("\\n");
     }
 
     public int add(String numbers) {
@@ -135,7 +135,10 @@ public class StringCalculator {
     }
 
     private String addCustomDelimiter(String numbers, String endCustomDelimiter) {
-        String[] splitDelimiter = numbers.split(endCustomDelimiter, 2);
+        // Escape special characters
+        String quote = Pattern.quote(endCustomDelimiter);
+
+        String[] splitDelimiter = numbers.split(quote, 2);
 
         String newDelimiter = splitDelimiter[0].substring(0, splitDelimiter[0].length());
 
